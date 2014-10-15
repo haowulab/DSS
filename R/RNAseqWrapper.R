0cafc030dd20287f082b95b861b990e1c209443d
Hao Wu <hao.wu@emory.edu>
2014-10-15 00:12:56 +0000

    DSS version 2.5.0
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DSS@95521 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/RNAseqWrapper.R
@@ -0,0 +1,19 @@
+################################################
+## A wrapper function for performing RNA-seq DE.
+## Use all default parameter values.
+################################################
+DSS.DE <- function(counts, design) {
+    ## some error checkings
+    if(! all(unique(design) %in% c(0,1)) )
+        stop("design must be a vector of 0 and 1.")
+    if(length(design) != ncol(counts))
+        stop("length of design doesn't match the number of columns of count matrix.")
+
+    seqData = newSeqCountSet(counts, designs)
+    seqData = estNormFactors(seqData)
+    seqData = estDispersion(seqData)
+    result = waldTest(seqData, 0, 1)
+    result
+}
+
+
