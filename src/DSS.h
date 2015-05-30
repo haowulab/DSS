9ce1c4db965041910a6e2bc7ad674d1b61fe8de4
Hao Wu <hao.wu@emory.edu>
2015-05-30 20:56:10 +0000

    DSS 2.8.0
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DSS@104390 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ src/DSS.h
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <R.h>
+#include <Rmath.h>
+#include <R_ext/PrtUtil.h>
+#include <Rinternals.h>
+#include <Rdefines.h>
+#include <R_ext/Rdynload.h>
+#define BINSIZE 50
+
+SEXP compute_var_smooth(SEXP vars, SEXP delta, SEXP n1, SEXP pos, SEXP ws, SEXP rho);
+SEXP windowFilter(SEXP x, SEXP pos, SEXP ws, SEXP R_flag);
+void compute_var_smooth_engine(double *vars, double *delta, double *n1, int *nitem,
+							   double *result_ptr, int npos, double rho, int *pos);
+void windowFilter_engine(double *x, int* nitem, int npos, int flag, double* result_ptr);
+void nitem_bin(SEXP pos_sexp, int ws, int* result_ptr);
+
+
