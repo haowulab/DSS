e5a8ad0e7df27862bead51069a6c5dd92f8198fb
Hao Wu <hao.wu@emory.edu>
2015-06-01 17:56:01 +0000

    DSS 2.8.0
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/DSS@104428 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/DSS.h
+++ src/DSS.h
@@ -12,8 +12,7 @@
 SEXP compute_var_smooth(SEXP vars, SEXP delta, SEXP n1, SEXP pos, SEXP ws, SEXP rho);
 SEXP windowFilter(SEXP x, SEXP pos, SEXP ws, SEXP R_flag);
 void compute_var_smooth_engine(double *vars, double *delta, double *n1, int *nitem,
-							   double *result_ptr, int npos, double rho, int *pos);
+			   double *result_ptr, int npos, double rho, int *pos);
 void windowFilter_engine(double *x, int* nitem, int npos, int flag, double* result_ptr);
 void nitem_bin(SEXP pos_sexp, int ws, int* result_ptr);
-
 
