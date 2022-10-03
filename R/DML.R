b09a9f38e3eefd0c7a4e1eff077c50e20a91187c
Hao Wu <hao.wu@emory.edu>
2022-10-03 14:03:56 -0400

    windows parallel computing

--- R/DML.R
+++ R/DML.R
@@ -11,8 +11,14 @@ DMLtest <- function(BSobj, group1, group2, equal.disp=FALSE, smoothing=FALSE,
                     smoothing.span=500, ncores) {
 
     ## determine number of cores used in parallel computing setting
-    if(missing(ncores))
-        ncores = max(detectCores() - 3, 1)
+    if(missing(ncores)) {
+        if(.Platform$OS.type == "windows" | Sys.info()['sysname'] == "Windows")
+            ## Windows, use single core
+            ncores = 1
+        else # for Mac and linux
+            ncores = max(detectCores() - 3, 1)
+    }
+
     if(ncores > detectCores())
         stop("ncores exceeds the number of CPU cores on the system.")
 
